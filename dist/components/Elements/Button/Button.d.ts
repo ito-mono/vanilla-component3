42be876fde2743c1f76239b33d3d8bbe1c9cd667
Toshiki Ito <ito@monoworks.co.jp>
2024-01-23 13:52:07 +0900

    delete dist

--- dist/components/Elements/Button/Button.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-export type ButtonProps = {
-    children: React.ReactNode;
-};
-export declare function Button({ children, ...props }: ButtonProps): import("react/jsx-runtime").JSX.Element;
